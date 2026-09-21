cc08141b6dd918f58e5941836493d90386290571
kiwah12 <kiwah110@gmail.com>
2026-09-21 19:15:58 +0800

    Update Rokok.java

--- NetBeansProjects/tugasPbo/src/main/java/model/Rokok.java
+++ NetBeansProjects/tugasPbo/src/main/java/model/Rokok.java
@@ -8,7 +8,7 @@ package model;
  *
  * @author MSI THIN 15
  */
-public class Rokok extends produk {
+public class Rokok extends Produk {
  
     public static final double MARGIN = 0.10;
     public static final double CUKAI_PER_BATANG = 250;
